3221590ea34b1c54dd5f9b4ebecf6d287a87c95d
LiuJun <1638814321@qq.com>
2021-06-26 20:49:52 +0800

    third-commit

--- src/com/atguigu/threadlocal/ThreadLocalTest.java
+++ src/com/atguigu/threadlocal/ThreadLocalTest.java
@@ -11,17 +11,17 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ThreadLocalTest {
 
-    public static final Map<String,Object> data = new ConcurrentHashMap<>(16);
+    public static final Map<String, Object> data = new ConcurrentHashMap<>(16);
     private static Random random = new Random();
 
 
-    public static class Task implements Runnable{
-        
+    public static class Task implements Runnable {
+
         @Override
         public void run() {
             //随机生成线程要关联的数据，以当前线程名作为KEY保存到MAP中
             int i = random.nextInt(1000);//0-999的一个随机数
-            Thread.currentThread().setName("线程"+i);
+            Thread.currentThread().setName("线程" + i);
             System.out.println("线程[" + Thread.currentThread().getName() + "]生成的随机数是：" + i);
             data.put(Thread.currentThread().getName(), i);
             try {
@@ -30,7 +30,12 @@ public class ThreadLocalTest {
                 e.printStackTrace();
             }
             Object o = data.get(Thread.currentThread().getName());
-            System.out.println("关联的数据是：" + o);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(o);
         }
     }
 
